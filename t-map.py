371311e918a8064a483f0527f8c37754cfef1f9e
MetallPunk88 <velikiy_bog@proton.me>
2026-05-08 03:30:31 +0700

    Update t-map.py

--- t-map.py
+++ t-map.py
@@ -88,7 +88,7 @@ def main():
             ip = input("Enter IP address: ").strip()
             if not is_valid_ip(ip):
                 continue
-
+#я педик
             ports = [21,22,23,25,53,80,110,135,139,143,443,445,993,995,1433,3306,3389,5432,5555,5900,6379,8080,8443]
             print(f"\nScanning {ip}...\n")
 
